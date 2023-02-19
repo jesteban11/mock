c78fa9228c6137ff5c26956faea33d352d966831
IngridC <juanmoncada11@gmail.com>
2023-02-19 18:01:32 -0500

    Initial Commit

---
+++ tests/test.spec.js
@@ -0,0 +1,13 @@
+describe('Test', () => {
+    let interceptRequests;
+    it('Open demoblaze', async () => {
+        interceptRequests = await browser.mock('**');
+        await browser.url('https://www.demoblaze.com/');
+    })
+
+    it('Intercept browser requests', async () => {
+        const demoBlazeApi = await browser.waitUntil(async () => await interceptRequests.calls.find((call) => call.url === 'https://api.demoblaze.com/entries'));
+        console.log(demoBlazeApi)
+
+    })
+})
